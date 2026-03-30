3f307bc14058ef8d218833abc30c3e67f820cc2f
Alexis DAMIEN <alexisdamien@icloud.com>
2026-03-30 11:46:12 +0200

    deleted old architecture

--- src/view/AppFireChange.js
+++
@@ -1,9 +0,0 @@
-export const AppFireChange = {
-  FireTextur(number) {
-    const strikNumber =
-      typeof number === "number" && !isNaN(number) ? Math.max(0, number) : 0;
-    const level = Math.min(Math.floor(strikNumber / 7) + 1, 12);
-
-    return `/assets/img/icons/strik/flame_${level}.png`;
-  },
-};
